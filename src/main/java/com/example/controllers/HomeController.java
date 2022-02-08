d88f0e5875bf1f35d44350e11dfac26b64dac3f1
mabrursatori <mabrursatori@gmail.com>
2022-02-08 14:30:47 +0700

    Create Master Page

--- src/main/java/com/example/controllers/HomeController.java
+++ src/main/java/com/example/controllers/HomeController.java
@@ -18,8 +18,8 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("")
 public class HomeController {
 
-	@Autowired
-	private ProductService productService;
+    @Autowired
+    private ProductService productService;
 
     @Autowired
     private HttpSession session;
@@ -27,43 +27,45 @@ public class HomeController {
     @Autowired
     private EmailService emailService;
 
-	@GetMapping
-	public String welcome(Model model) {
+    @GetMapping
+    public String welcome(Model model) {
         emailService.sendEmail("mabrursatori@gmail.com", "New Email", "Hallo Bro!");
         System.out.println("TEST HOME");
-		String message = "Welcome to SPRING MVC";
-		model.addAttribute("message", message);
+        String message = "Welcome to SPRING MVC";
+        model.addAttribute("message", message);
         model.addAttribute("searchForm", new SearchFormData());
-		model.addAttribute("products", productService.findAll());
-		return "index";
-	}
+        model.addAttribute("products", productService.findAll());
+        return "index";
+    }
 
     @GetMapping("/add")
-    public String add(Model model){
+    public String add(Model model) {
+        model.addAttribute("searchForm", new SearchFormData());
         model.addAttribute("product", new Product());
         return "add";
     }
 
     @PostMapping("/save")
-    public String save(Product product, Model model){
+    public String save(Product product, Model model) {
         productService.addProduct(product);
         return "redirect:/";
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id") Long id){
+    public String delete(@PathVariable("id") Long id) {
         productService.deleteById(id);
         return "redirect:/";
     }
 
     @GetMapping("/edit/{id}")
-    public String edit(@PathVariable("id") Long id, Model model){
+    public String edit(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("searchForm", new SearchFormData());
         model.addAttribute("product", productService.findById(id));
         return "edit";
     }
 
     @PostMapping("/update")
-    public String update(Product product, Model model){
+    public String update(Product product, Model model) {
         productService.updateProduct(product);
         return "redirect:/";
     }
